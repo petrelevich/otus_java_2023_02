2ccb98bee040a7f93303049a7e99fdaf50b6da10
petrelevich <petrelevich@yandex.ru>
2023-04-20 15:23:20 +0300

    L15-structuralPatterns

---
+++ L15-structuralPatterns/homework/build.gradle.kts
@@ -0,0 +1,9 @@
+
+dependencies {
+    testImplementation ("org.junit.jupiter:junit-jupiter-api")
+    testImplementation ("org.junit.jupiter:junit-jupiter-engine")
+    testImplementation ("org.junit.jupiter:junit-jupiter-params")
+    testImplementation ("org.assertj:assertj-core")
+    testImplementation ("org.mockito:mockito-core")
+    testImplementation ("org.mockito:mockito-junit-jupiter")
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,24 +1,26 @@
 rootProject.name = "otusJava"
-include ("L01-gradle")
+include("L01-gradle")
 
-include ("L02-gradle2")
-include ("L02-gradle2-libApi")
-include ("L02-gradle2-libApiUse")
-include ("L03-qa")
-include ("L04-generics")
-include ("L05-collections")
-include ("L06-annotations")
+include("L02-gradle2")
+include("L02-gradle2-libApi")
+include("L02-gradle2-libApiUse")
+include("L03-qa")
+include("L04-generics")
+include("L05-collections")
+include("L06-annotations")
 
 include("L08-gc:demo")
 include("L08-gc:homework")
 
 include("L09-docker")
-include ("L10-byteCodes")
-include ("L11-java8")
+include("L10-byteCodes")
+include("L11-java8")
 
-include ("L12-solid")
-include ("L13-creationalPatterns")
-include ("L14-behavioralPatterns")
+include("L12-solid")
+include("L13-creationalPatterns")
+include("L14-behavioralPatterns")
+include("L15-structuralPatterns:demo")
+include("L15-structuralPatterns:homework")
 
 
 pluginManagement {
