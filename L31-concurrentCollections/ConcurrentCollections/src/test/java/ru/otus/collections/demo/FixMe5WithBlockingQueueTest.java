2d6d17097b0c1616aa01402302da0f7b763b72eb
petrelevich <petrelevich@yandex.ru>
2023-07-06 09:38:13 +0300

    L31 concurrent collections

--- L31-concurrentCollections/ConcurrentCollections/src/test/java/ru/otus/collections/demo/FixMe5WithBlockingQueueTest.java
+++ L31-concurrentCollections/ConcurrentCollections/src/test/java/ru/otus/collections/demo/FixMe5WithBlockingQueueTest.java
@@ -1,15 +1,19 @@
 package ru.otus.collections.demo;
 
+import java.util.Random;
+import java.util.concurrent.ArrayBlockingQueue;
+import java.util.concurrent.BlockingQueue;
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.CountDownLatch;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-//TODO please FIXME with BlockingQueue
+//DEMO with BlockingQueue
 // Вопросы:
 // - Что делает это многопоточное приложение?
 // - Какие есть проблемы в данном многопоточном приложении?
@@ -17,12 +21,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 // - Фиксим тест сейчас!
 // - *Для какого сценария по нагрузке больше всего подходит BlockingQueue?
 class FixMe5WithBlockingQueueTest {
+    private static final Logger log = LoggerFactory.getLogger(FixMe5WithBlockingQueueTest.class);
     private static final int ITERATIONS_COUNT = 1000;
+    private final Random random = new Random();
 
     @Test
     void testBlockingQueueWorksGreat() throws InterruptedException {
 
-        List<Integer> list = new ArrayList<>();
+        BlockingQueue<Integer> list = new ArrayBlockingQueue<>(5);
         final CountDownLatch latch = new CountDownLatch(2);
         List<Exception> exceptions = new CopyOnWriteArrayList<>();
 
@@ -31,7 +37,12 @@ class FixMe5WithBlockingQueueTest {
                 latch.countDown();
                 latch.await();
                 for (int i = 0; i < ITERATIONS_COUNT; i++) {
-                    list.remove(0);
+                    var result = list.poll();
+                    if (result != null) {
+                        log.info("element:{}", result);
+                    } else {
+                        sleep();
+                    }
                 }
             } catch (Exception ex) {
                 exceptions.add(ex);
@@ -43,7 +54,10 @@ class FixMe5WithBlockingQueueTest {
                 latch.countDown();
                 latch.await();
                 for (int i = 0; i < ITERATIONS_COUNT; i++) {
-                    list.add(list.size(), 5);
+                    if (!list.offer(i)) {
+                        log.info("skiped element:{}", i);
+                        sleep();
+                    }
                 }
             } catch (Exception ex) {
                 exceptions.add(ex);
@@ -56,5 +70,13 @@ class FixMe5WithBlockingQueueTest {
         t1.join();
         t2.join();
         assertThat(exceptions).withFailMessage(exceptions.toString()).isEmpty();
+    }
+
+    private void sleep() {
+        try {
+            Thread.sleep(random.nextInt(10 - 1) + 1);
+        } catch (InterruptedException e) {
+            Thread.currentThread().interrupt();
+        }
     }
 }
