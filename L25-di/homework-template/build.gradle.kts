e8c1ce95af29187745522b8a7474ff88fc47f20b
Alex Klepov <alexezm@yandex.ru>
2023-12-10 18:51:14 +0300

    base part

--- L25-di/homework-template/build.gradle.kts
+++ L25-di/homework-template/build.gradle.kts
@@ -1,6 +1,6 @@
 dependencies {
     implementation("org.reflections:reflections")
-
+    implementation("org.javatuples:javatuples:1.2")
     testImplementation("org.junit.jupiter:junit-jupiter-engine")
     testImplementation("org.junit.jupiter:junit-jupiter-params")
     testImplementation("org.assertj:assertj-core")
